015317eb87ed5128ae4ac28b1eba86aa8a921d62
hkimmarusys <hkim@marusys.com>
2025-08-29 18:02:14 +0900

    Delete model_test directory

--- model_test/train_and_convert.py
+++
@@ -1,8 +0,0 @@
-import json
-
-with open("2D_dataset.json", "r") as f:
-    data = json.load(f)
-
-print("keys:", data.keys())
-print("len(train_X):", len(data["train_X"]))
-print("len(train_Y):", len(data["train_Y"]))
